41692107ccf64c08abc35f250f2d3d0606f1cb23
viniciusth <viniciustht@gmail.com>
2021-03-07 22:31:38 -0300

    Fixing search bar href

--- assets/static/assets/script.js
+++ assets/static/assets/script.js
@@ -4,12 +4,12 @@ function func(assets, input){
   var new_html = ""
   for(var i=0; i < assets.length; i++){
     if(assets[i].match("[A-Z0-9]*" + input + "[A-Z0-9]*") != null){
-      new_html += '<a href="/assets/"' + assets[i] + '/>' + assets[i] + "</a>";
+      new_html += '<a href="/assets/' + assets[i] + '"/>' + assets[i] + "</a>";
       new_html += '\n\n';
     }
   }
   tickers.innerHTML = new_html;
-  //console.log(tickers.innerHTML)
+  console.log(tickers.innerHTML)
   //console.log(assets)
   //console.log(input)
 }
